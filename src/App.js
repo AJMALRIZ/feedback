f6e440fd28d8dc09863a14e27507e14d28cf3cf2
webforums <muhammedajmalriswan@gmail.com>
2022-06-12 13:55:14 +0530

    to masterdeploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 
 import FeedbackList from './components/FeedbackList'
 import Header from './components/Header'
-import feedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
